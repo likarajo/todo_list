5c2bc84519871bf6f87baf6bf0c4f93d1bce2a9f
rchattopadhyay <rchattopadhyay@tesla.com>
2020-08-13 23:04:16 -0700

    Updates

--- precache-manifest.77806450e5c1348a897fc92f6b076824.js
+++ precache-manifest.77806450e5c1348a897fc92f6b076824.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9865a4f85a34a73d2bda33b0d7828008",
+    "revision": "66c3f9dee503395835b9e95fe8ee199a",
     "url": "/todo_list/index.html"
   },
   {
-    "revision": "74373699f07023790ffc",
+    "revision": "20762cbac978113f04c2",
     "url": "/todo_list/static/css/main.e473579b.chunk.css"
   },
   {
-    "revision": "468f9c4dfa7c5064d329",
-    "url": "/todo_list/static/js/2.354bc41a.chunk.js"
+    "revision": "e790b871d3fcffb017a2",
+    "url": "/todo_list/static/js/2.260bcb74.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/todo_list/static/js/2.354bc41a.chunk.js.LICENSE.txt"
+    "url": "/todo_list/static/js/2.260bcb74.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "74373699f07023790ffc",
-    "url": "/todo_list/static/js/main.848379bb.chunk.js"
+    "revision": "20762cbac978113f04c2",
+    "url": "/todo_list/static/js/main.34129bb3.chunk.js"
   },
   {
     "revision": "22129c5b33155a530639",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/todo_list/precache-manifest.f6204e8bebb256a598de710b06997ad5.js"
+  "/todo_list/precache-manifest.77806450e5c1348a897fc92f6b076824.js"
 );
 
 self.addEventListener('message', (event) => {
