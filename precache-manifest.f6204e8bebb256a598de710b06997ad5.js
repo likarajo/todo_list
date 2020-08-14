0ba9ecab39aa27c381ee0c92c64f3284f5a87147
rchattopadhyay <rchattopadhyay@tesla.com>
2020-08-13 17:04:53 -0700

    Updates

---
+++ precache-manifest.f6204e8bebb256a598de710b06997ad5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9865a4f85a34a73d2bda33b0d7828008",
+    "url": "/todo_list/index.html"
+  },
+  {
+    "revision": "74373699f07023790ffc",
+    "url": "/todo_list/static/css/main.e473579b.chunk.css"
+  },
+  {
+    "revision": "468f9c4dfa7c5064d329",
+    "url": "/todo_list/static/js/2.354bc41a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo_list/static/js/2.354bc41a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74373699f07023790ffc",
+    "url": "/todo_list/static/js/main.848379bb.chunk.js"
+  },
+  {
+    "revision": "22129c5b33155a530639",
+    "url": "/todo_list/static/js/runtime-main.5cd06204.js"
+  }
+]);
